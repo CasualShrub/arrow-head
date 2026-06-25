95a9b6fb5c6d4eb06c1337d9e255c978deeaab40
Iamtheepicado <charlie.lette@gmail.com>
2026-06-26 07:43:06 +1000

    implement

--- scripts/arrow_pattern.gd
+++ scripts/arrow_pattern.gd
@@ -3,6 +3,7 @@ class_name ArrowPattern
 
 @export var weight: float = 1.0
 @export var recovery: float = 0.0
+@export var uses_windup := false
 @export_group("movement")
 @export var has_movement_pattern: bool = false
 @export var movement_pattern: Dictionary[float, Vector2] = {}
