9212a5e0124dc429d85495565f26d1e0e0f43f52
Eric Lee <elee0558@uni.sydney.edu.au>
2026-06-26 05:15:14 +1000

    level enemy updates

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -352,7 +352,7 @@ func _on_sus_alerted() -> void:
 	_recovery.start()
 	var p = get_parent()
 	for e in p.get_children():
-		if e.global_position.distance_to(global_position) < 20.0 and not e.sus.is_alert(): 
+		if e.global_position.distance_to(global_position) < 10.0 and not e.sus.is_alert(): 
 			e.sus.baka(1.1)
 
 func _on_health_changed() -> void:
